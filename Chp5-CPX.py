2f29074a7ad3942c05232b6eb5f7a3c5a37a26f1
Jackie Merritt <remmerritt@gmail.com>
2025-06-29 21:46:05 -0400

    Need help

--- Chp5-CPX.py
+++ Chp5-CPX.py
@@ -1,24 +1,43 @@
 from adafruit_circuitplayground import cp
 import time
 
-#Highest value: 321
-#Lowest value: 0
-#35.67 repeating
-#0 = 0
-#1-35 = A1 36-70 = A2 71-105 = A3 106-140 = A4
-#141-175 = A5 176-210 = A6 211-245 = A7 246-280 = A8
-#281-321 = A9
+# Highest value: 321
+# Lowest value: 0
+# 35.67 repeating
+# 0 = 0
+# 1-35 = A1 36-70 = A2 71-105 = A3 106-140 = A4
+# 141-175 = A5 176-210 = A6 211-245 = A7 246-280 = A8
+# 281-321 = A9
 
-def scale(value):
-    index = int(321 * 9)
-    return index
+# index = Neopixels 0-9
+# lightValue is the value of the light detected by the sensor
+# MAX is the maximum value shown by the sensor
+# Maxindex = 9 or highest neopixel
 
-def black():
-    for num in range(0, 9):
+
+maxlight = 321
+maxneo = 10
+
+def main():
+    while True:
+        if cp.light:
+            light = cp.light
+            index = scale(light)
+            cp.pixels[index] = (0, 50, 0)
+        # else:
+            off()
+
+def scale(light):
+    return int(light/maxlight * maxneo)
+
+def off():
+    for num in range(0, 10):
         cp.pixels[num] = (0, 0, 0)
 
+main()
 
-while True:
-    light = cp.light
-    print((light,))
-    time.sleep(.05)
+
+# while True:
+    # light = cp.light
+    # print((light,))
+    # time.sleep(.05)
